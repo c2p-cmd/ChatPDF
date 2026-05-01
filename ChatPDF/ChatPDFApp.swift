39b81917afe193656d6780f161795f2b1640fb27
Sharan Thakur <sharandt19@live.com>
2026-05-01 13:16:54 +0200

    Initial Commit

---
+++ ChatPDF/ChatPDFApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatPDFApp.swift
+//  ChatPDF
+//
+//  Created by Sharan Thakur on 01/05/26.
+//
+
+import SwiftUI
+
+@main
+struct ChatPDFApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
